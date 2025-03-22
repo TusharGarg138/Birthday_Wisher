7aef8ec2ff3610838e726465c555675a90877039
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-22 12:04:06 +0530

    main

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import smtplib
+import datetime as dt
 
-my_email = "agarwaltushar3710@gmail.com"
-password =  "ohhn vfpe ozih yccf"
+#my_email = "agarwaltushar3710@gmail.com"
+#password =  "ohhn vfpe ozih yccf" # chmage this with yahoo password generating
+#
+#with smtplib.SMTP("smtp.gmail.com") as connection: # this too server
+#    connection.starttls()
+#    connection.login(user= my_email, password=password )
+#    connection.sendmail(
+#        from_addr = my_email,
+#        to_addrs = "gargtushar138@yahoo.com",
+#        msg= "Subject:hello\n\n this is the body of my email"
+#    )
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user= my_email, password=password )
-    connection.sendmail(
-        from_addr = my_email,
-        to_addrs = "gargtushar138@yahoo.com",
-        msg= "Subject:hello\n\n this is the body of my email"
-    )
+
+now = dt.datetime.now()
+year = now.year
+day_of_week = now.weekday()
+print(now)
+print(day_of_week)
+
+date_of_birth = dt.datetime(year= 2005, month=1, day= 5)
+print(date_of_birth)
