cf864a6a6f2258a4fb7d65dfa10de58454eb06d8
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-22 15:52:39 +0530

    main

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import smtplib
 import datetime as dt
+import random
 
-#my_email = "agarwaltushar3710@gmail.com"
-#password =  "ohhn vfpe ozih yccf" # chmage this with yahoo password generating
-#
+my_email = "agarwaltushar3710@gmail.com"
+my_pass = "ohhn vfpe ozih yccf"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 5:
+    with open("text.txt") as file:
+        all_quotes = file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, my_pass)
+        connection.sendmail(
+            from_addr = my_email,
+            to_addrs = my_email,
+            msg = f"Subject: Monday motivation \n\n {quote}")
+
+
+
+
+
+
+# basic knowleadge of the libraries :->
+
+
 #with smtplib.SMTP("smtp.gmail.com") as connection: # this too server
 #    connection.starttls()
 #    connection.login(user= my_email, password=password )
@@ -14,11 +39,11 @@ import datetime as dt
 #    )
 
 
-now = dt.datetime.now()
-year = now.year
-day_of_week = now.weekday()
-print(now)
-print(day_of_week)
+#now = dt.datetime.now()
+#day_of_week = now.weekday()
+#year = now.year
+#print(now)
+#print(day_of_week)
 
-date_of_birth = dt.datetime(year= 2005, month=1, day= 5)
-print(date_of_birth)
+#date_of_birth = dt.datetime(year= 2005, month=1, day= 5)
+#print(date_of_birth)
